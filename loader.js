5c41bb295e12680807fa9012528d91d288e3c9d1
SaNDS Lab <53471331+ajitsands@users.noreply.github.com>
2024-03-14 23:12:38 +0530

    loader.js

---
+++ loader.js
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+// add below code when the event strats
+$('#loadingWrapper').show();  
+
+
+// add below code when the event finish
+$('#loadingWrapper').hide();  
